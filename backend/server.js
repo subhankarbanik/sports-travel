21c588704c7c61e43f9c54b02088fbfd81ca9e3c
subhankarbanik <subhankarbanik4msbu@gmail.com>
2025-11-29 23:20:41 +0530

    erro

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-// app.use(cors());
-app.use(
-    cors({
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
-    })
-  );
+app.use(cors());
+
 app.use(express.json());
 
 let leads = [];
